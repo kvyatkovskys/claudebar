52f6c9ea2fee789923a4bf54b7b95187c0a0d098
Vladimir Babin <vovababin@gmail.com>
2026-04-14 21:54:52 +0300

    release: v0.0.1

--- Sources/Services/UpdateChecker.swift
+++ Sources/Services/UpdateChecker.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct UpdateChecker {
     static let repo = "chiliec/claudebar"
-    static let currentVersion = "1.0.0"
+    static let currentVersion = "0.0.1"
 
     struct Release: Codable {
         let tagName: String
